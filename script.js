55716756ac52aeeac7f10566f1997a1278745636
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-20 16:12:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
   //your code here
-  
+   const sortedArr = arr.slice().sort((a, b) => a - b);
+    let totalCost = 0;
+
+    while (sortedArr.length > 1) {
+        const min1 = sortedArr.shift();
+        const min2 = sortedArr.shift();
+        const cost = min1 + min2;
+        totalCost += cost;
+        sortedArr.push(cost);
+        sortedArr.sort((a, b) => a - b);
+    }
+
+    return totalCost;
   
   
 }  
+const arr = [4, 2, 7, 6, 9];
